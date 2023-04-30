ac8ed7deee2f083a386062ba515adeee79944f2c
Mohammed FIROS <mohammedfiros20@gmail.com>
2023-04-30 10:53:55 +0530

    APPJS for prod path fixed

--- server/app.js
+++ server/app.js
@@ -2,12 +2,18 @@ import  express  from "express";
 import dotenv from 'dotenv';
 import ErrerMiddleware from './midlewares/errer.js'
 import cookieparser from "cookie-parser"
-import cors from  'cors'
+import path from "path"
+import {fileURLToPath} from 'url'
+// import cors from  'cors'
+
+
+const _filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(_filename)
 
 dotenv.config({path:'./config/.env'});
 
 const app= express();
-import path from "path"
+
 // const _dirname = path.dirname('')
 // const buildpath = path.join(_dirname ,"../cousebundler/build/") ;
 
@@ -20,9 +26,9 @@ import path from "path"
 // })
 
 //
-const __dirname = path.dirname('')
+// const __dirname = path.dirname('')
 const buildPath = path.join(__dirname, '../cousebundler/build/');
-console.log(buildPath);
+// console.log(buildPath,"dadadsadasd");
 const indexPage = path.join(buildPath, '/index.html');
 
 app.use(express.static(buildPath));
@@ -41,6 +47,7 @@ app.get('/', (req,res)=>{
 //     methods:["GET","POST","DELETE","PUT",]
 
 // }))
+
 app.use(cookieparser());
 
 //using midleares
